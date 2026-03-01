9b45af91a22fc83d6eed2efa132cc77f9c5174d6
ltreptegit <ltrepteschool@gmail.com>
2026-02-28 20:09:29 -0500

    Added Simulink and Matlab script to set up simulin

---
+++ RockySimulinkVariables.m
@@ -0,0 +1,11 @@
+g = 9.81;
+l = 0.4873;   %effective length 
+a = 23.2135;           %nominal motor parameters
+b = 0.003; 
+omegan = 4.4869;
+Kp = 57744;
+Ki = 269020;
+Jp = 0.12;
+Ji = 0.13;
+Ci = 0.14;
+%simulink
