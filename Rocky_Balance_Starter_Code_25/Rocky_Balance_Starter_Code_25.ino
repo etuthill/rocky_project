c5c72137268d4bea715609c1b83452d8a8dcbf84
etuthill <etuthillolin@gmail.com>
2026-03-11 17:23:38 -0400

    latest version

--- Rocky_Balance_Starter_Code_25/Rocky_Balance_Starter_Code_25.ino
+++ Rocky_Balance_Starter_Code_25/Rocky_Balance_Starter_Code_25.ino
@@ -28,18 +28,18 @@ extern int32_t distanceRight;
 float speedCont = 0;
 float displacement_m = 0;
 int16_t limitCount = 0;
-uint32_t cur_time = 0;
-float distLeft_m;
-float distRight_m;
+uint32_t cur_time = 0;  
+float distLeft_m;       
+float distRight_m;       
 
 
 extern uint32_t delta_ms;
 float measured_speedL = 0;
 float measured_speedR = 0;
-float desSpeedL = 0;
-float desSpeedR = 0;
+float desSpeedL=0;
+float desSpeedR =0;
 float dist_accumL_m = 0;
-float dist_accumR_m = 0;
+float dist_accumR_m = 0; 
 float dist_accum = 0;
 float speed_err_left = 0;
 float speed_err_right = 0;
@@ -50,13 +50,13 @@ float errAccumLeft_m = 0;
 float prevDistLeft_m = 0;
 float prevDistRight_m = 0;
 float angle_rad_diff = 0;
-float angle_rad;            // this is the angle in radians
-float angle_rad_accum = 0;  // this is the accumulated angle in radians
-float angle_prev_rad = 0;   // previous angle measurement
+float angle_rad;                // this is the angle in radians
+float angle_rad_accum = 0;      // this is the accumulated angle in radians
+float angle_prev_rad = 0; // previous angle measurement
 extern int32_t displacement;
-int32_t prev_displacement = 0;
+int32_t prev_displacement=0;
 uint32_t prev_time;
-
+ 
 #define G_RATIO (162.5)
 
 
@@ -68,7 +68,7 @@ Balboa32U4Buzzer buzzer;
 Balboa32U4ButtonA buttonA;
 
 
-#define FIXED_ANGLE_CORRECTION (0.0) // ***** Replace the value 0.25 with the value you obtained from the Gyro calibration procedure
+#define FIXED_ANGLE_CORRECTION (0.014)  // ***** Replace the value 0.25 with the value you obtained from the Gyro calibration procedure
 
 
 
@@ -80,95 +80,55 @@ Balboa32U4ButtonA buttonA;
 // You should make modifications to this function to perform your
 // balancing
 //////////////////////////////////////////////////////////////////////////////////////////////
+void BalanceRocky()
+{
+  float Kp = 487.2487;
+  float Ki = 2177.2;
+  float Ci = -38.5176;
+  float Jp = -178.2512;
+  float Ji = -72.5415;
 
-void BalanceRocky() {
+  float v_c_L, v_c_R;
 
-  // **************Enter the control parameters here
+  float v_d = Kp * angle_rad + Ki * angle_rad_accum;
 
-  float Kp = 633.3f;      // angle proportional gain
-  float Ki = 2841.7f;     // angle integral gain
-  float Ji = -108.8f;     // speed integral gain
-  float Jp = -148.1f;     // speed proportional gain
-  float Ci = -43.9f;      // distance integral gain
+  speed_err_left  = v_d - measured_speedL;
+  speed_err_right = v_d - measured_speedR;
 
+  speed_err_left_acc  += speed_err_left * 0.01;
+  speed_err_right_acc += speed_err_right * 0.01;
 
+  v_c_R = v_d + Jp * speed_err_right + Ji * speed_err_right_acc + Ci * dist_accum;
+  v_c_L = v_d + Jp * speed_err_left  + Ji * speed_err_left_acc  + Ci * dist_accum;
 
-  float v_c_L, v_c_R;  // these are the control velocities to be sent to the motors
-  float v_d = 0;       // this is the desired speed produced by the angle controller
+  if(v_c_L > 300) v_c_L = 300;
+  if(v_c_R > 300) v_c_R = 300;
+  if(v_c_L < -300) v_c_L = -300;
+  if(v_c_R < -300) v_c_R = -300;
 
-
-  // Variables available to you are:
-  // angle_rad  - angle in radians
-  // angle_rad_accum - integral of angle
-  // measured_speedR - right wheel speed (m/s)
-  // measured_speedL - left wheel speed (m/s)
-  // distLeft_m - distance traveled by left wheel in meters
-  // distRight_m - distance traveled by right wheel in meters  (this is the integral of the velocities)
-  // dist_accum - integral of the distance
-
-  // *** enter an equation for v_d in terms of the variables available ****
-  v_d = Kp * angle_rad + Ki * angle_rad_accum;  // this is the desired velocity from the angle controller
-
-
-  // The next two lines implement the feedback controller for the motor. Two separate velocities are calculated.
-  //
-  //
-  // We use a trick here by criss-crossing the distance from left to right and
-  // right to left. This helps ensure that the Left and Right motors are balanced
-
-  // *** enter equations for input signals for v_c (left and right) in terms of the variables available ****
-  v_c_R = v_d + Jp * (v_d - measured_speedR) + Ji * speed_err_right_acc + Ci * dist_accum;
-  v_c_L = v_d + Jp * (v_d - measured_speedL) + Ji * speed_err_left_acc + Ci * dist_accum;
-
-
-
-
-  // save desired speed for debugging
-  desSpeedL = v_c_L;
-  desSpeedR = v_c_R;
-
-  // the motor control signal has to be between +- 300. So clip the values to be within that range
-  // here
-  if (v_c_L > 300) v_c_L = 300;
-  if (v_c_R > 300) v_c_R = 300;
-  if (v_c_L < -300) v_c_L = -300;
-  if (v_c_R < -300) v_c_R = -300;
-
-  // Set the motor speeds
-  motors.setSpeeds((int16_t)(v_c_L), (int16_t)(v_c_R));
+  motors.setSpeeds((int16_t)v_c_L,(int16_t)v_c_R);
 }
 
 
-
-void setup() {
-
+void setup()
+{
+  // Uncomment these lines if your motors are reversed.
   motors.flipLeftMotor(true);
   motors.flipRightMotor(true);
 
   Serial.begin(9600);
-
-  Wire.begin();
-  if (!imu.init())
-  {
-    Serial.println("IMU failed");
-    while(1);
-  }
-  imu.enableDefault();
-
   prev_time = 0;
   displacement = 0;
-
   ledYellow(0);
   ledRed(1);
-
   balanceSetup();
-
   ledRed(0);
   angle_accum = 0;
 
   ledGreen(0);
-  ledYellow(0);
+  ledYellow(0);  
 }
+
 
 
 int16_t time_count = 0;
@@ -179,7 +139,8 @@ void lyingDown();
 extern bool isBalancingStatus;
 extern bool balanceUpdateDelayedStatus;
 
-void UpdateSensors() {
+void UpdateSensors()
+{
   static uint16_t lastMillis;
   uint16_t ms = millis();
 
@@ -189,148 +150,137 @@ void UpdateSensors() {
 
   // call functions to integrate encoders and gyros
   balanceUpdateSensors();
-
-  imu.read();
-if (imu.a.x > 0) {
-  isBalancingStatus = true;
-} else {
-  lyingDown();
-  isBalancingStatus = false;
-}
-}
-
-
-
-void GetMotorAndAngleMeasurements() {
-  // convert distance calculation into meters
-  // and integrate distance
-  distLeft_m = ((float)distanceLeft) / ((float)G_RATIO) / 12.0 * 80.0 / 1000.0 * 3.14159;
-  distRight_m = ((float)distanceRight) / ((float)G_RATIO) / 12.0 * 80.0 / 1000.0 * 3.14159;
-  dist_accum += (distLeft_m + distRight_m) * 0.01 / 2.0;
-
-  // compute left and right wheel speed in meters/s
-  measured_speedL = speedLeft / ((float)G_RATIO) / 12.0 * 80.0 / 1000.0 * 3.14159 * 100.0;
-  measured_speedR = speedRight / ((float)G_RATIO) / 12.0 * 80.0 / 1000.0 * 3.14159 * 100.0;
-
-  prevDistLeft_m = distLeft_m;
-  prevDistRight_m = distRight_m;
-
-
-  // this integrates the angle
-  angle_rad_accum += angle_rad * 0.01;
-  // this is the derivative of the angle
-  angle_rad_diff = (angle_rad - angle_prev_rad) / 0.01;
-  angle_prev_rad = angle_rad;
-}
-
-void balanceResetAccumulators() {
-  errAccumLeft_m = 0.0;
-  errAccumRight_m = 0.0;
-  speed_err_left_acc = 0.0;
-  speed_err_right_acc = 0.0;
+ 
+  if (imu.a.x > 0)
+  {
+    lyingDown();
+    isBalancingStatus = false;
+  }
+  else
+  {
+    isBalancingStatus = true;
+  }
 }
 
 
 
-void loop() {
-  static uint32_t prev_print_time = 0;  // this variable is to control how often we print on the serial monitor
-  static uint32_t prev_diag_time = 0;
-  int16_t distanceDiff;                 // this stores the difference in distance in encoder clicks that was traversed by the right vs the left wheel
+void GetMotorAndAngleMeasurements()
+{
+    // convert distance calculation into meters
+    // and integrate distance
+    distLeft_m = ((float)distanceLeft)/((float)G_RATIO)/12.0*80.0/1000.0*3.14159;       
+    distRight_m = ((float)distanceRight)/((float)G_RATIO)/12.0*80.0/1000.0*3.14159;       
+    dist_accum += (distLeft_m+distRight_m)*0.01/2.0;
+
+    // compute left and right wheel speed in meters/s
+    measured_speedL = speedLeft/((float)G_RATIO)/12.0*80.0/1000.0*3.14159*100.0;
+    measured_speedR = speedRight/((float)G_RATIO)/12.0*80.0/1000.0*3.14159*100.0;
+    
+    prevDistLeft_m = distLeft_m;
+    prevDistRight_m = distRight_m;
+
+
+    // this integrates the angle
+    angle_rad_accum += angle_rad*0.01;
+    // this is the derivative of the angle
+    angle_rad_diff = (angle_rad-angle_prev_rad)/0.01;
+    angle_prev_rad  = angle_rad;
+ 
+}
+
+void balanceResetAccumulators()
+{
+    errAccumLeft_m = 0.0;
+    errAccumRight_m = 0.0;
+    speed_err_left_acc = 0.0;
+    speed_err_right_acc = 0.0;
+}
+
+
+
+void loop()
+{
+  static uint32_t prev_print_time = 0;   // this variable is to control how often we print on the serial monitor
+  int16_t distanceDiff;    // this stores the difference in distance in encoder clicks that was traversed by the right vs the left wheel
   static float del_theta = 0;
   char enableLongTermGyroCorrection = 1;
+  
+  cur_time = millis();                   // get the current time in miliseconds
 
 
-
-  cur_time = millis();  // get the current time in miliseconds
-
-
-  if ((cur_time - prev_time) > UPDATE_TIME_MS) {
-    UpdateSensors();  // run the sensor updates.
-
+  if((cur_time - prev_time) > UPDATE_TIME_MS){
+    UpdateSensors();                    // run the sensor updates.
+    
     // calculate the angle in radians. The FIXED_ANGLE_CORRECTION term comes from the angle calibration procedure (separate sketch available for this)
     // del_theta corrects for long-term drift
-    angle_rad = ((float)angle) / 1000 / 180 * 3.14159 - FIXED_ANGLE_CORRECTION - del_theta;
-
-    if (angle_rad > 0.1 || angle_rad < -0.1)  // If angle is not within +- 6 degrees, reset counter that waits for start
+    angle_rad = ((float)angle)/1000/180*3.14159 - FIXED_ANGLE_CORRECTION - del_theta;
+    
+    if(angle_rad > 0.1 || angle_rad < -0.1)      // If angle is not within +- 6 degrees, reset counter that waits for start
     {
       start_counter = 0;
-    }
+   }
 
-
-    if (angle_rad > -0.1 && angle_rad < 0.1 && !start_flag) {
-      // increment the start counter
-      start_counter++;
-      // If the start counter is greater than 30, this means that the angle has been within +- 6 degrees for 0.3 seconds, then set the start_flag
-      if (start_counter > 30) {
-        balanceResetEncoders();
-        start_flag = 1;
-        buzzer.playFrequency(DIV_BY_10 | 445, 1000, 15);
-        Serial.println("Starting");
-        ledYellow(1);
-      }
-    }
-
-
-    // every UPDATE_TIME_MS, if the start_flag has been set, do the balancing
-    if (start_flag) {
-      GetMotorAndAngleMeasurements();
-      if (enableLongTermGyroCorrection)
-        del_theta = 0.999 * del_theta + 0.001 * angle_rad;  // assume that the robot is standing. Smooth out the angle to correct for long-term gyro drift
-
-      // Control the robot
-      BalanceRocky();
-    }
-    prev_time = cur_time;
-  }
-  // if the robot is more than 45 degrees, shut down the motor
-  if (start_flag && angle_rad > .78) {
-    motors.setSpeeds(0, 0);
-    start_flag = 0;
-  } else if (start_flag && angle_rad < -0.78) {
-    motors.setSpeeds(0, 0);
-    start_flag = 0;
-  }
-
-  // kill switch
-  if (buttonA.getSingleDebouncedPress()) {
-    motors.setSpeeds(0, 0);
-    while (!buttonA.getSingleDebouncedPress())
-      ;
-  }
-
-  if (cur_time - prev_print_time > 103)  // do the printing every 105 ms. Don't want to do it for an integer multiple of 10ms to not hog the processor
+  
+  if(angle_rad > -0.1 && angle_rad < 0.1 && ! start_flag)   
   {
-    Serial.print(angle_rad);
-    Serial.print("\t");
-    Serial.print(distLeft_m);
-    Serial.print("\t");
-    Serial.print(measured_speedL);
-    Serial.print("\t");
-    Serial.print(measured_speedR);
-    Serial.print("\t");
-    Serial.println(speedCont);
-    prev_print_time = cur_time;
+    // increment the start counter
+    start_counter++;
+    // If the start counter is greater than 30, this means that the angle has been within +- 6 degrees for 0.3 seconds, then set the start_flag
+    if(start_counter > 30)
+    {
+      balanceResetEncoders();
+      start_flag = 1;
+      buzzer.playFrequency(DIV_BY_10 | 445, 1000, 15);
+      Serial.println("Starting");
+      ledYellow(1);
+    }
   }
 
-  if (cur_time - prev_diag_time > 100)
+
+  // every UPDATE_TIME_MS, if the start_flag has been set, do the balancing
+  if(start_flag)
   {
-    Serial.print("angle_rad: ");
-    Serial.print(angle_rad,5);
-
-    Serial.print("  start_counter: ");
-    Serial.print(start_counter);
-
-    Serial.print("  start_flag: ");
-    Serial.print(start_flag);
-
-    Serial.print("  imu.ax: ");
-    Serial.print(imu.a.x);
-
-    Serial.print("  balancing: ");
-    Serial.print(isBalancingStatus);
-
-    Serial.println();
-
-    prev_diag_time = cur_time;
+    GetMotorAndAngleMeasurements();
+    if(enableLongTermGyroCorrection)
+      del_theta = 0.999*del_theta + 0.001*angle_rad;  // assume that the robot is standing. Smooth out the angle to correct for long-term gyro drift
+    
+    // Control the robot
+    BalanceRocky();
   }
+  prev_time = cur_time;
+  }  
+// if the robot is more than 45 degrees, shut down the motor
+  if(start_flag && angle_rad > .78)
+  {
+    motors.setSpeeds(0,0);
+    start_flag = 0;
+  }
+  else if(start_flag && angle_rad < -0.78)
+  {
+    motors.setSpeeds(0,0);
+    start_flag = 0;
+  } 
+
+// kill switch
+  if(buttonA.getSingleDebouncedPress())
+  {
+      motors.setSpeeds(0,0);
+      while(!buttonA.getSingleDebouncedPress());
+  }
+
+if(cur_time - prev_print_time > 103)   // do the printing every 105 ms. Don't want to do it for an integer multiple of 10ms to not hog the processor
+  {
+        Serial.print(angle_rad);   
+        Serial.print("\t");
+        Serial.print(distLeft_m);
+        Serial.print("\t");
+        Serial.print(measured_speedL);
+        Serial.print("\t");      
+        Serial.print(measured_speedR);
+        Serial.print("\t");      
+       Serial.println(speedCont);
+       prev_print_time = cur_time;
+  }
+
+ 
 }
